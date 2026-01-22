33056c59278a396978f0ed52086c78696bd151b0
hjsandy-stack <hjspurrier@gmail.com>
2026-01-21 22:09:35 -0500

    Assignment 1

---
+++ First.java/First.java
@@ -0,0 +1,10 @@
+//Haley Sandoval
+public class First {
+    public static void main(String[] args)
+    {   System.out.println("Hola mundo!");
+        System.out.println("Job title: Information Secuirty Analysts");
+        System.out.println("Why: Ability to move around and work remotely as well as market growth");
+        System.out.println("Median salary: $124,910");
+
+    }
+}
