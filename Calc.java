991bb98a803ad01673c97833162b179ecd97a7fd
hjsandy-stack <hjspurrier@gmail.com>
2026-03-16 17:20:30 -0400

    updated calc and runner files

--- Calc.java
+++ Calc.java
@@ -1,46 +1,61 @@
+import java.util.Scanner;
+
 public class Calc {
 
     private double num1;
     private double num2;
 
-    public Calc(){
+    public Calc() {
         num1 = 0;
         num2 = 0;
     }
 
-    public void setNum1(double num1){
+    public void setNum1(double num1) {
         this.num1 = num1;
     }
 
-    public void setNum2(double num2){
+    public void setNum2(double num2) {
         this.num2 = num2;
     }
 
-    public double getNum1(){
+    public double getNum1() {
         return num1;
     }
 
-    public double getNum2(){
+    public double getNum2() {
         return num2;
     }
 
-    public double add(){
+    public double add() {
         return num1 + num2;
     }
 
-    public double subtract(){
+    public double subtract() {
         return num1 - num2;
     }
 
-    public double multiply(){
+    public double multiply() {
         return num1 * num2;
     }
 
-    public double divide(){
+    public double divide() {
         return num1 / num2;
     }
 
-    public String toString(){
+    public double getValidNumber(Scanner scan, String prompt) {
+        while (true) {
+            System.out.println(prompt);
+
+            if (scan.hasNextDouble()) {
+                return scan.nextDouble();
+            } else {
+                System.out.println("Invalid input. Please enter a numeric value.");
+                scan.next(); // clears the bad input
+            }
+        }
+    }
+
+    public String toString() {
         return "Num1: " + num1 + "\nNum2: " + num2;
     }
 }
